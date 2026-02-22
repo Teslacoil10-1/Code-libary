d3a84b5d020da3296c5434a7c40861a73392ab24
Nathaniel Whittingham <nrossiw@gmail.com>
2026-02-22 17:45:11 +0000

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-
+import { Analytics } from "@vercel/analytics/next"
 document.documentElement.classList.add('dark')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Analytics/>
   </BrowserRouter>
 )
 
